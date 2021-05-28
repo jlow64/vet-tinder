1dd215d68a424915e636a6afc69d51e6905a13e8
jlow64 <justin64low@gmail.com>
2021-05-28 13:19:21 +1200

    Made login page content

--- frontend/my-app/src/components/loginPage.js
+++ frontend/my-app/src/components/loginPage.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import '../componentscss/LoginPage.css'
 
-const loginPage = () => {
-    return <div>Login Page</div>;
+const LoginPage = () => {
+    return (
+        <div className='login-div'>
+            <Form>
+            <h1>Vet Matcher</h1>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                Please log in with your email address.
+                </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </div>
+    );
 };
 
-export default loginPage;
+export default LoginPage;

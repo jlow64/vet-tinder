dc52210bc509fc2f0dc2a326d19710dd092d3226
jlow64 <justin64low@gmail.com>
2021-05-29 09:40:11 +1200

    completed general navigation

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,28 +1,38 @@
 import React, { useState } from 'react'
-import LoginPage from './components/loginPage'
+import LoginPage from './components/LoginPage'
 import FilterModal from './components/FilterModal'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
-
 import './App.css'
 
 function App() {
   const [emailValid, setEmailValid] = useState(false);
+  const [address, setAddress] = useState('');
+  const [radius, setRadius] = useState(5);
   function emailValidTrue() {
     setEmailValid(true);
   }
   function emailValidFalse() {
     setEmailValid(false);
   }
+  function passAddress(address) {
+    setAddress(address);
+  }
+  function passRadius(radius) {
+    setRadius(radius);
+  }
 
   if (!emailValid) {
-    return <LoginPage emailValidTrue={emailValidTrue} />
+    return <div className='App'><LoginPage emailValidTrue={emailValidTrue} /></div>
   }
 
   return (
     <div className="App">
       <Router>
           <Route exact path="/">
-          <FilterModal emailValidFalse={emailValidFalse}/>
+          <FilterModal emailValidFalse={emailValidFalse} passAddress={passAddress} passRadius={passRadius} />
+          </Route>
+          <Route path="/Search">
+           <div>{radius}</div>
           </Route>
       </Router>
     </div>

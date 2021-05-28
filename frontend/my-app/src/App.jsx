52c309493ff98e2bbae1377856e3213cc178ab90
jlow64 <justin64low@gmail.com>
2021-05-28 14:13:54 +1200

    filtermodal component created

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import LoginPage from './components/LoginPage'
+import FilterModal from './components/FilterModal'
+import { Map, GoogleApiWrapper, Marker  } from 'google-maps-react';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
+      <FilterModal />
     </div>
   );
 }

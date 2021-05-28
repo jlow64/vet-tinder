f3939c49a6594d476e4f9e8f3a2c77c722ffaa95
RUTH HA <ruthha@Ruths-MacBook-Air-2.local>
2021-05-28 22:42:40 +1200

    updated list style, get request

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import LoginPage from './components/LoginPage'
+import LoginPage from './components/loginPage'
 import FilterModal from './components/FilterModal'
+import List from './components/List'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 import './App.css'
@@ -14,6 +15,9 @@ function App() {
           </Route>
           <Route exact path="/FilterModal">
             <FilterModal />
+          </Route>
+          <Route exact path="/List">
+            <List />
           </Route>
       </Router>
     </div>
--- frontend/my-app/src/components/List.jsx
+++ frontend/my-app/src/components/List.jsx
@@ -1,21 +1,38 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import Button from 'react-bootstrap/Button';
 import vets from './../../src/vets.json';
-import './../components.css/List.css'
 import axios from 'axios';
+import './../../src/componentscss/List.css'
 console.log(vets)
 
 const List = () => {
+    const [data, setData] = useState('');
+    useEffect(() => {
+        // GET request fetches list of places
+        axios.get('http://localhost:3000/places')
+            .then(response => setData(response.data.total));
+    }, []);
     
     return (
     <div id='vetList'>
+        <h1> List of Places </h1>
         <CardColumns>
         {
             vets.map(vet=> {
-                return <Card bg ="light" border="secondary" style={{ width: '18rem' }}> <Card.Body><Card.Title> {vet.name} </Card.Title>
-                <Card.Text> <div className="address"> {vet.vicinity}</div></Card.Text> </Card.Body></Card>
+                return <Card bg ="light" border="secondary" style={{ width: '18rem' }}> 
+                <Card.Body>
+                    <Card.Title>
+                        <div className="vetTitle">{vet.name} </div> 
+                    </Card.Title>
+                    <Card.Text> 
+                        <div className="address"> {vet.vicinity}</div>
+                    </Card.Text> 
+                    <Button variant="info">Book</Button>
+                </Card.Body>
+                </Card>
             })
         }
         </CardColumns>
